532a2b42f1d61a49d8a25faf6be841c3d3c4dc57
likwidmack <likwidmack@gmail.com>
2019-12-31 02:37:16 -0800

    Container cleanup & formatting

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -5,7 +5,7 @@ import { Word } from '../Display';
 import './Container.scss';
 
 const count = 1;
-const getWordUrl = `https://tmack-api-staging.herokuapp.com/api/random/words/${ count }?length=5-15`;
+const getWordUrl = `https://tmack-api-staging.herokuapp.com/api/random/words/${count}?length=5-15`;
 
 export function Container(props) {
   let o = {},
@@ -14,13 +14,12 @@ export function Container(props) {
   class Container extends React.Component {
     letterArray = [];
     completion = false;
+    attempts = 0;
 
     constructor(props) {
       super(props);
       this.state = {
-        attempts: 0,
         character: false,
-        completion: false,
         disabled: false,
         word: false,
       };
@@ -31,67 +30,66 @@ export function Container(props) {
     }
 
     render() {
-      let { attempts, character, disabled, word } = this.state;
-      let { completion, letterArray } = this;
+      let { character, disabled, word } = this.state;
+      let { attempts, completion, letterArray } = this;
 
       return (
-        <div className='grid-y medium-grid-frame container-wrapper'>
-          <div className='cell shrink header medium-cell-block-container'>
-            <div className='grid-x grid-padding-x'>
-              <div id='header-alerts'
-                   className={ `cell auto ${ completion ? '' : completion }` }>
+        <div className="grid-y medium-grid-frame container-wrapper">
+          <div className="cell shrink header medium-cell-block-container">
+            <div className="grid-x grid-padding-x">
+              <div id="header-alerts" className={`cell auto ${completion ? '' : completion}`}>
                 <h1>React Hangman</h1>
               </div>
-              <div className='cell shrink header-bar'>
-                <div className='grid-x grid-margin-x'>
-                  <div className='cell shrink callout'>
-                    { character }
-                  </div>
-                  <div className='cell auto callout'>
+              <div className="cell shrink header-bar">
+                <div className="grid-x grid-margin-x">
+                  <div className="cell shrink callout">{character}</div>
+                  <div className="cell auto callout">
                     <p>Attempts remaining</p>
-                    <div className='stat'>{ 10 - attempts }</div>
+                    <div className="stat">{10 - attempts}</div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className='cell medium-auto medium-cell-block-container'>
-            <div className='grid-x grid-padding-x'>
-              <div className='cell medium-4 medium-cell-block-y'>
-                <Form word={ word }
-                      isDisabled={ disabled }
-                      inputHandler={ e => this.updateCharacter(e) }
-                      resetHangman={ bool => this.updateWord(bool) }/>
+          <div className="cell medium-auto medium-cell-block-container">
+            <div className="grid-x grid-padding-x">
+              <div className="cell medium-4 medium-cell-block-y">
+                <Form
+                  word={word}
+                  isDisabled={disabled}
+                  inputHandler={e => this.updateCharacter(e)}
+                  resetHangman={bool => this.updateWord(bool)}
+                />
                 <div>
-                  { completion && completion === 'fail' ? (
-                    <div className='callout alert hangman-results'>
+                  {completion && completion === 'fail' ? (
+                    <div className="callout alert hangman-results">
                       <h4>Correct Answer</h4>
-                      <span>{ word }</span>
-                    </div>) : null }
-                  { completion && completion === 'success' ? (
-                    <div className='callout success hangman-results'>
+                      <span>{word}</span>
+                    </div>
+                  ) : null}
+                  {completion && completion === 'success' ? (
+                    <div className="callout success hangman-results">
                       <h4>Correct! Congratulations!</h4>
-                    </div>) : null }
+                    </div>
+                  ) : null}
                 </div>
               </div>
-              <div className='cell medium-8 medium-cell-block-y'>
+              <div className="cell medium-8 medium-cell-block-y">
                 <div className={`hangman-container callout ${completion ? completion : ''}`}>
-                  <Hangman incorrectGuessCount={ attempts }/>
-                  <Word characters={ letterArray }
-                        completion={ completion }/>
+                  <Word characters={letterArray} completion={completion} />
+                  <Hangman incorrectGuessCount={attempts} />
+                  <Word characters={letterArray} completion={completion} />
                 </div>
               </div>
             </div>
           </div>
-          <div className='cell shrink footer'>
-            <div className='grid-x grid-padding-x'>
-              <div className='cell shrink'>
-                <h4 className='subheader'>Characters Used</h4>
+          <div className="cell shrink footer">
+            <div className="grid-x grid-padding-x">
+              <div className="cell shrink">
+                <h4 className="subheader">Characters Used</h4>
               </div>
-              <div className='cell auto'>
-                <p className='callout secondary'>
-                  { [...history].sort().join(' ') }
-                </p>
+              <div className="cell auto">
+                <p className="callout secondary">{[...history].sort().join(' ')}</p>
               </div>
             </div>
           </div>
@@ -104,10 +102,9 @@ export function Container(props) {
     }
 
     set currentLetter(character) {
-      let { attempts } = this.state;
       character = character || false;
 
-      const letter = o[ character ];
+      const letter = o[character];
       console.log('currentLetter', character, letter, o);
 
       if (history.has(character)) {
@@ -119,38 +116,42 @@ export function Container(props) {
       if (letter) {
         letter.done = true;
         let indices = letter.i.split(',');
-        while (indices.length) this.letterArray[ +indices.pop() ] = character;
+        while (indices.length) this.letterArray[+indices.pop()] = character;
       } else {
-        attempts += 1;
-        if (isNaN(attempts) || attempts > 10) attempts = 10;
+        this.attempts += 1;
+        if (isNaN(this.attempts) || this.attempts > 10) this.attempts = 10;
       }
 
-      this.updateCompletion(attempts);
-      this.setState({ attempts, character });
+      this.updateCompletion();
+      this.setState({ character });
     }
 
     get store() {
-      return o;
+      return Object.assign({}, o);
     }
 
     set store(word) {
       o = {};
       for (let i = 0; i < word.length; i++) {
-        const char = word[ i ];
-        if (!o[ char ]) o[ char ] = {
-          i: `${ i }`,
-          done: false,
-        };
-        else o[ char ].i += `,${ i }`;
+        const char = word[i];
+        if (!o[char])
+          o[char] = {
+            i: `${i}`,
+            done: false,
+          };
+        else o[char].i += `,${i}`;
       }
     }
 
     reset(word) {
       history = new Set();
       this.store = word;
-      this.letterArray = Array(word.length).fill(null).map(v => '');
+      this.letterArray = Array(word.length)
+        .fill(null)
+        .map(v => '');
       this.completion = false;
-      this.setState({ attempts: 0, character: false });
+      this.attempts = 0;
+      this.setState({ character: false });
     }
 
     updateCharacter(event) {
@@ -167,14 +168,13 @@ export function Container(props) {
       if (!this.completion) target.focus();
     }
 
-    updateCompletion(attempts) {
+    updateCompletion() {
       let { disabled } = this.state;
 
       disabled = true;
-      if (attempts < 10 &&
-        Object.values(this.store).every(v => v.done)) {
+      if (this.attempts < 10 && Object.values(o).every(v => v.done)) {
         this.completion = 'success';
-      } else if (attempts >= 10) {
+      } else if (this.attempts >= 10) {
         this.completion = 'fail';
       } else {
         disabled = false;
@@ -198,7 +198,9 @@ export function Container(props) {
       fetch(getWordUrl)
         .then(res => res.json())
         .then(data => {
-          const { words: [word] } = data;
+          const {
+            words: [word],
+          } = data;
           this.reset(word);
           this.setState({
             disabled: false,
@@ -210,8 +212,7 @@ export function Container(props) {
           this.setState({ disabled: false });
         });
     }
-
   }
 
-  return (<Container { ...props } />);
+  return <Container {...props} />;
 }
